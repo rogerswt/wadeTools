34cf49772aa7d58419acbbecb2d17a2ba8dc64bb
Wade Rogers <rogersw@upenn.edu>
2020-01-17 14:12:12 -0500

    added functions to clean up nice naming, and to facilitate round-tripping of FCS data to/from disk.

--- R/ff_importutils.R
+++ R/ff_importutils.R
@@ -31,6 +31,9 @@
 #' @export
 get_sample = function(fn, compensate=TRUE, transform=TRUE, derail=TRUE, nice.names = TRUE, verbose=FALSE) {
 
+
+  requireNamespace("flowCore")
+
   ff = read.FCS(fn)
   if (verbose) {message("reading")}
   fl_params = which(flowCore::colnames(ff) %in% colnames(keyword(ff)$SPILL))
@@ -49,20 +52,7 @@ get_sample = function(fn, compensate=TRUE, transform=TRUE, derail=TRUE, nice.nam
   }
   if (nice.names) {
     if (verbose) {message("nice names")}
-    dnames = flowCore::colnames(ff)    # detector names
-
-    names = parameters(ff)$desc
-    names[sc_params] = flowCore::colnames(ff)[sc_params]
-    scat_names = names[sc_params]
-    fl_names = names[fl_params]
-    parameters(ff)$desc[sc_params] = scat_names
-
-    # handle any other names, like "Time"
-    other_params = which(is.na(parameters(ff)$desc))
-    other_names = flowCore::colnames(ff)[other_params]
-
-    flowCore::colnames(ff)[c(sc_params, fl_params, other_params)] = c(scat_names, fl_names, other_names)
-    parameters(ff)$desc[c(sc_params, fl_params, other_params)] = c(scat_names, paste(dnames[fl_params], fl_names), other_names)
+    ff = swap.names(ff)
   }
 
   ff
@@ -86,6 +76,9 @@ get_sample = function(fn, compensate=TRUE, transform=TRUE, derail=TRUE, nice.nam
 #' @return The transformed flowFrame.
 #' @export
 doTransform <- function(ff,cols = c(1:5, 7:13),method = c("biexp","log","linear"), fac = 5.4/262143) {
+
+  requireNamespace("flowCore")
+
   if (is(ff,"flowFrame")) {
     method = match.arg(method)
     if (method == "biexp") {
@@ -128,11 +121,137 @@ doTransform <- function(ff,cols = c(1:5, 7:13),method = c("biexp","log","linear"
 
 }
 
-swapNames = function(ff,capNames=F){
-  ind = which(!is.na(parameters(ff)$desc))
-  flowCore::colnames(ff)[ind] = parameters(ff)$desc[ind]
-  if (capNames) {
-    flowCore::colnames(ff)[ind] = toupper(colnames(ff)[ind])
+#' @title Detect Scattering Parameters in a flowFrame
+#' @description Return the indices of the scattering parameters.
+#' @param ff The flowFrame to be processed
+#'
+#' @details This function will work with files whose scattering parameter names
+#' include "FSC" or "SSC".  Not all instruments name parameters thusly.
+#' @return The indices corresponding to the scattering parameters.
+#' @export
+detect_scatter_parameters = function(ff) {
+
+  requireNamespace("flowCore")
+
+  sc_params = which(grepl(pattern = "FSC", x = flowCore::colnames(ff)) | grepl(pattern = "SSC", x = flowCore::colnames(ff)))
+
+  sc_params
+}
+
+
+#' @title Detect Fluorescence Parameters in a flowFrame
+#' @description Return the indices of the fluorescence parameters.
+#' @param ff The flowFrame to be processed
+#' @param non.fl Names of all non-fluorescence parameters in the flowFrame
+#' @details This function assumes that FL parameters are anything that don't contain
+#' (non-case-sensitive) "fsc", "ssc", "time", "clean" or "index", or any other
+#' parameter names supplied.
+#' in the argument, non.fl.
+#' @return The indices corresponding to the scattering parameters.
+#' @export
+detect_fl_parameters = function(ff, non.fl = c("fsc", "ssc", "time", "clean", "index")) {
+
+  requireNamespace("flowCore")
+
+  all.names = colnames(ff)
+  idx = vector(mode = 'numeric')
+  for (i in 1:length(non.fl)) {
+    idx = append(idx, which(grepl(pattern = non.fl[i], x = all.names, ignore.case = TRUE)))
   }
-  return(ff)
+  fl_params = (1:ncol(ff))[-idx]
+
+  fl_params
+}
+
+#' @title Re-arrange Parameter Labeling in a flowFrame
+#' @description Exchange operator-supplied parameter labels (in the pData 'desc' field)
+#'  with the configuration-dependent parameter names (in the pData 'name' field).
+#' @param ff The flowFrame to be processed
+#'
+#' @details Generally, exchange 'names' with 'desc' in pData(parameters(ff)).
+#' Note that the colnames in the SPILL matrix are adjusted so that autocomp
+#' should still work.
+#' @return A flowFrame with re-arranged labels.
+#' @export
+swap.names = function(ff) {
+
+  requireNamespace("flowCore")
+
+  sc_params = detect_scatter_parameters(ff)
+  fl_params = detect_fl_parameters(ff)
+
+  # replicate scattering param names first
+  parameters(ff)$desc[sc_params] = parameters(ff)$name[sc_params]
+
+  # swap fl names
+  dname = parameters(ff)$name[fl_params]
+  pname = parameters(ff)$desc[fl_params]
+  parameters(ff)$desc[fl_params] = dname
+  colnames(ff)[fl_params] = pname
+  parameters(ff)$name[fl_params] = pname
+
+  # replicate any others left
+  idx = which(is.na(parameters(ff)$desc))
+  parameters(ff)$desc[idx] = parameters(ff)$name[idx]
+
+  ff
+}
+
+#' @title Replace range data in the pData(parameters(ff)) slot
+#' @description We found that round-tripping a flowFrame by first \code{ \link[flowCore]{write.FCS}}
+#' followed by \code{ \link[flowCore]{read.FCS}} caused weird effects in the phenodata
+#' of the resulting flowFrame.  This function replaces the screwed up range numbers
+#' with ones stored from before the read-back.
+#' @param ff The flowFrame to be processed
+#' @param range The range values to replace those in ff
+#' @param minRange The minRange values to replace those in ff
+#' @param maxRange The maxRange values to replace those in ff
+#' @details This function should be used with care.  The range values are affected
+#' by the per-parameter transformations that may or may not have been previously
+#' applied.  There is currently no good mechanism to track them.
+#'
+#' The default values of NULL for range, minRange and maxRange result in the generation
+#' of values appropriate for data that have been (a) linearly scaled to 0-5.4
+#' on scattering parameters, and (b) biexponentially transformed (using \code{ \link{biexpTransform}})
+#' on the fluorescence parameters.  Otherwise, care should be taken to supply
+#' range, minRange and maxRange vectors having the same length as ncol(ff).
+#' @return A flowFrame with replace range values.
+#' @export
+replace_phedata_ranges = function(ff, range = NULL, minRange = NULL, maxRange = NULL) {
+
+  requireNamespace("flowCore")
+
+  fl_params = detect_fl_parameters(ff)
+  sc_params = detect_scatter_parameters(ff)
+  time_param = which(grepl(pattern = 'time', x = colnames(ff), ignore.case = TRUE))
+
+  # generate ranges
+  if (is.null(range)) {
+    range = vector(mode = 'numeric')
+    range[sc_params]  = 5.4
+    range[fl_params]  = 5.4185
+    range[time_param] = max(exprs(ff)[, time_param])
+  }
+
+  if (is.null(maxRange)) {
+    maxRange = vector(mode = 'numeric')
+    maxRange[sc_params]   = 5.4
+    maxRange[fl_params]   = 5.4185
+    maxRange[time_param] = max(exprs(ff)[, time_param])
+  }
+
+  if (is.null(minRange)) {
+    minRange = vector(mode = 'numeric')
+    minRange[sc_params] = 0.0
+    minRange[fl_params] = -0.1715
+    minRange[time_param] = 0.0
+  }
+
+  # apply ranges to ff
+  parameters(ff)$range = range
+  parameters(ff)$minRange = minRange
+  parameters(ff)$maxRange = maxRange
+
+  # return the ff
+  ff
 }
