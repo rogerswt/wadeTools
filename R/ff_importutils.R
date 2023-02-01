5a53d73813680bd39f1526b45613e7faf904fdd5
Wade Rogers <rogersw@upenn.edu>
2023-02-01 16:53:37 -0500

    "feature" introduced in flowCore 2.10.0, causing some FCS files to be truncated due to the Time parameter exceeding the $PnR spec.

--- R/ff_importutils.R
+++ R/ff_importutils.R
@@ -34,7 +34,8 @@ get_sample = function(fn, compensate = TRUE, transform = TRUE, derail = TRUE, ni
 
   requireNamespace("flowCore")
 
-  ff = suppressWarnings(read.FCS(fn, truncate_max_range = FALSE))  # changes resulting for upgrading to R4.0
+  ff = suppressWarnings(read.FCS(fn, truncate_max_range = FALSE,
+                                 truncate_max_range = FALSE))  # changes resulting for upgrading to flowCore 2.10.0
   if (verbose) {message("reading")}
   fl_params = which(flowCore::colnames(ff) %in% colnames(keyword(ff)$SPILL))
   sc_params = which(grepl(pattern = "FSC", x = flowCore::colnames(ff)) | grepl(pattern = "SSC", x = flowCore::colnames(ff)))
